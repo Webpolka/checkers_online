74a75421f26f11c139d296168ab2240aaecae4ce
Webpolka <webpolka@gmail.com>
2026-02-04 20:38:28 +0300

    04.02.2026 20:19

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { AppButton } from "@/components/ui/appButton";
+
+export const NotFoundPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="relative h-screen w-screen flex flex-col items-center justify-center text-white px-6 text-center">
+
+            {/* ===== Фоновое изображение ===== */}
+            <img
+                src="/images/rooms-bg.webp"
+                alt="Фон шашек"
+                className="absolute inset-0 w-full h-full object-cover -z-10"
+            />
+
+            {/* ===== Полупрозрачный градиент сверху ===== */}
+            <div className="absolute inset-0 bg-gradient-to-b from-blue-500/40 via-blue-600/40 to-indigo-700/40 -z-9"></div>
+
+
+            <h1 className="text-[140px] leading-[1.1] tracking-[0.3em] font-extrabold mb-0">404</h1>
+            <p className="text-2xl font-semibold mb-8">Страница не найдена</p>
+            <AppButton variant="primary" onClick={() => navigate("/welcome")}>
+                Вернуться на главную
+            </AppButton>
+        </div>
+    );
+};
